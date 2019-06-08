866f60600ff1e0db404de17ab0ce8a70a59fe4e3
admin <zacharyjsanders@gmail.com>
2019-06-07 21:32:23 -0600

    test

---
+++ my-default-starter/src/components/header.js
@@ -0,0 +1,49 @@
+import { Link } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      background: `#bf622a`,
+      marginBottom: `1.45rem`,
+    }}
+  >
+    <div
+      style={{
+        margin: `0 auto`,
+        maxWidth: 960,
+        padding: `1.45rem 1.0875rem`,
+      }}
+    >
+      <h1 style={{ margin: 0 }}>
+        <Link
+          to="/"
+          style={{
+            color: `white`,
+            textDecoration: `none`,
+          }}
+        >
+          {siteTitle}
+        </Link>
+      </h1>
+    </div>
+    <div class="wilderNav">
+      <Link to="/about/">About</Link>
+      <Link to="/services/">Services</Link>
+      <Link to="/portfolio/">Portfolio</Link>
+      <Link to="">Hot Off The Press</Link>
+      <Link to="/contact/">Contact</Link>
+    </div>
+  </header>
+)
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
